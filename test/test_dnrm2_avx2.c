8b82e0ebd2e6916914730e1f16c3fe9669c0154e
yzhaiustc <yzhai015@ucr.edu>
2022-12-06 14:51:38 -0800

    dnrm2_avx2, test

--- test/test_dnrm2_avx2.c
+++ test/test_dnrm2_avx2.c
@@ -4,81 +4,60 @@
 #include "../include/utils.h"
 #include "../include/ftblas.h"
 
+// linking with Intel oneMKL
+#include "mkl.h"
 
 int main(int argc, char* argv[])
 {
-    int m = 20000;
     int inc_x = 1;
-    if (argc != 2) 
-    {
-        printf("Please input m:\n");
-        printf("Wrong input, exit.\n");
-        exit(-1);
-    }
 
-    m = atoi(argv[1]);
-
-    if (m == 0) {
-        printf("vector length cannot be ZERO, return.\n");
-        return -1;
-    }
-
-//    m = 2 << m;
-    printf("m = %d\n", m);
-
-    if (inc_x <= 0) {
-        printf("increment of vector should be positive, return.\n");
-        return -1;
-    }
-
-    double *vec_x, *vec_y;
-    double *vec_a, *vec_b;
-    double *vec_m, *vec_n;
+    double *vec_x;
     double t0, t1, res_ft, res_ori;
+    double elapsed_time;
 
-    vec_x = (double *)malloc(sizeof(double) * m * 1);
-    vec_y = (double *)malloc(sizeof(double) * m * 1);
-    vec_a = (double *)malloc(sizeof(double) * m * 1);
-    vec_b = (double *)malloc(sizeof(double) * m * 1);
-    vec_m = (double *)malloc(sizeof(double) * m * 1);
-    vec_n = (double *)malloc(sizeof(double) * m * 1);
+    int SIZE[21];
+    for (int i = 0; i < 21; i++) {
+        SIZE[i] = (int)(1e6 + i * 1e5);
+    }
 
-    randomize_matrix(vec_x, m, 1);
-    //randomize_matrix(vec_y, m, 1);
-    //copy_matrix(vec_y, vec_n, m);
-    //print_vector(vec_x, m);
-/*
-    printf("\n################# BLAS #################\n");
-    fflush(stdout);
-    t0 = get_sec();
-    res_ori = cblas_dnrm2(m, vec_x, inc_x);
-    t1 = get_sec();
-    printf("CBLAS : Elapsed time: %8.6fs, Perf: %8.6f \n", t1-t0, \
-        2 * (1 / 1000. / inc_x) * (m / 1000.) * (1 / 1000.) / (t1 - t0)) ;    
-    fflush(stdout);
-*/
-    printf("\n################# FT #################\n");
-    fflush(stdout);
-    t0 = get_sec();
-    res_ft = ft_dnrm2(m, vec_x, inc_x);
-    t1 = get_sec();
-    printf("ft-blas : Elapsed time: %8.6fs, Perf: %8.6f \n", t1-t0, \
-        2 * (1 / 1000. / inc_x) * (m / 1000.) * (1 / 1000.) / (t1 - t0)); 
-    fflush(stdout);
-
-    //print_vector(vec_x, m);
-
-
-    //print_vector(vec_a, m);
-
+    int upper_limit = (sizeof(SIZE) / sizeof(int));
+    int max_size = SIZE[upper_limit - 1];
+    const int TEST_COUNT = 20;
+    vec_x = (double *)malloc(sizeof(double) * max_size * 1);
+    randomize_matrix(vec_x, max_size, 1);
     
+    for (int i_count = 0; i_count < upper_limit; i_count++) {
+        int m = SIZE[i_count];
+        
+        printf("\nTesting M = %d:\n",m);
+        
+        res_ori = cblas_dnrm2(m, vec_x, inc_x);
+        res_ft = ft_dnrm2(m, vec_x, inc_x);
+
+        double diff = res_ori - res_ft;
+        
+        if (fabs(diff) > 1e-3) {
+            printf("Failed to pass the correctness verification against Intel oneMKL. Exited.\n");
+            exit(-1);
+        }else{
+            printf("Passed the sanity check, start benchmarking the performance.\n");
+        }
+        
+        t0 = get_sec();
+        
+        for (int t_count = 0; t_count < TEST_COUNT; t_count++) {
+            // we dont need the result so we don't take the return val here.
+            ft_dnrm2(m, vec_x, inc_x);
+        }
+
+        t1 = get_sec();
+        elapsed_time = t1 - t0;
+
+        printf("Average elasped time: %f second, performance: %f GFLOPS.\n", \
+            elapsed_time/TEST_COUNT, 2.*1e-9*TEST_COUNT * m / elapsed_time);
+    }
 
     free(vec_x);
-    free(vec_y);
-    free(vec_a);
-    free(vec_b);
-    free(vec_m);
-    free(vec_n);
 
     return 0;
 }
