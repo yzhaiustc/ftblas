528f8a32a56e5ea94367b4eb7d6156d9b6ea4525
Vineeth Suvarna <vineeth22@live.in>
2023-02-06 11:51:07 -0800

    Fixed typo

--- src/cblas_dsbmv.cpp
+++ src/cblas_dsbmv.cpp
@@ -1,17 +1,17 @@
 #include "../include/ftblas.h"
 #include "../include/cblas.h"
 
-void cblas_sbmv_compute(const CBLAS_UPLO Uplo, const FTBLAS_INT n, const FTBLAS_INT k, const double alpha, const double *a, const FTBLAS_INT lda, const double *x,
+void cblas_dsbmv_compute(const CBLAS_UPLO Uplo, const FTBLAS_INT n, const FTBLAS_INT k, const double alpha, const double *a, const FTBLAS_INT lda, const double *x,
                         const int incx, const double beta, const double *y, const int incy);
 
-void cblas_sbmv(const CBLAS_UPLO uplo, const FTBLAS_INT n, const FTBLAS_INT k, const double alpha, const double *a, const FTBLAS_INT lda, const double *x,
+void cblas_dsbmv(const CBLAS_UPLO uplo, const FTBLAS_INT n, const FTBLAS_INT k, const double alpha, const double *a, const FTBLAS_INT lda, const double *x,
                 const int incx, const double beta, const double *y, const int incy)
 {
 
-    cblas_sbmv_compute(uplo, n, k, alpha, a, lda, x, incx, beta, y, incy);
+    cblas_dsbmv_compute(uplo, n, k, alpha, a, lda, x, incx, beta, y, incy);
 }
 
-void cblas_sbmv_compute(const CBLAS_UPLO Uplo, const FTBLAS_INT n, const FTBLAS_INT k, const double alpha, const double *a, const FTBLAS_INT lda, const double *x,
+void cblas_dsbmv_compute(const CBLAS_UPLO Uplo, const FTBLAS_INT n, const FTBLAS_INT k, const double alpha, const double *a, const FTBLAS_INT lda, const double *x,
                         const int incx, const double beta, const double *y, const int incy)
 {
     int uplo;
